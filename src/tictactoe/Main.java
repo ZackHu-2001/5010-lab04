2bbea2486552639d0c9bfa25f9c0b163f36ba77d
ZackHu <144851646+ZackHu-2001@users.noreply.github.com>
2023-10-13 21:35:34 -0700

    modify the comment

--- src/tictactoe/Main.java
+++ src/tictactoe/Main.java
@@ -11,30 +11,10 @@ public class Main {
    * Run a TicTacToe game interactively on the console.
    */
   public static void main(String[] args) {
-//    Readable input = new InputStreamReader(System.in);
-//    StringReader input = new StringReader("7.5 1 1 1 1 2 2 2 1 1 7 1 2 3 1");
-    StringReader input = new StringReader("2 2 1 1 3 3 1 2 1 3 2 3 2 1 3 1 3 2");
-//    StringReader input = new StringReader("0 1 1 1 2 2 3 3 2 2 hello 2 3 1 3 2 1");
+    Readable input = new InputStreamReader(System.in);
     Appendable output = System.out;
     new TicTacToeConsoleController(input, output).playGame(new TicTacToeModel());
   }
 }
 
-
-//    2 2 1 1 3 3 1 2 1 3 2 3 2 1 3 1 3 2
-//    1 1 2 2 1 3 1 2 3 3 1 3 q
-//    1 1 2 2 1 3 1 2 3 3 3 2
-//    2 2 1 2 1 1 2 1 3 3
-//    1 1 2 2 1 3 2 q
-//    0 1 1 1 2 2 3 3 2 2 hello 2 3 1 3 2 1
-//    7.5 1 1 1 1 2 2 2 1 1 7 1 2 3 1
-//    1 1 4 2 q
-//    2 2 q
-//    1 1 2 4 q
-//    3 8.79 q
-//    1 1 2 2 1 3 q
-//    2 2 1 3 9 2 u 2 2 6 2 1 q
-//    0 1 1 1 2 2 3 3 2 2 hello 2 3 1 3 2 1
-//    1 1 2 2 1 1 1 2 1 3 5 2 3 1 2 1 fhgy 2 3 2 9 3 2 3 3
-//    !#$ 2 q
 
