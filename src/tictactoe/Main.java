30c13faad7f7bd6ec66c95fa95de3c28b32102f6
ZackHu <144851646+ZackHu-2001@users.noreply.github.com>
2023-10-13 23:39:13 -0700

    add test

--- src/tictactoe/Main.java
+++ src/tictactoe/Main.java
@@ -5,10 +5,13 @@ import java.io.StringReader;
 
 /**
  * Run a TicTacToe game interactively on the console.
+ *
  */
 public class Main {
   /**
-   * Run a TicTacToe game interactively on the console.
+   * The `main` method is the entry point for running the game.
+   *
+   * @param args Command-line arguments (not used in this application).
    */
   public static void main(String[] args) {
     Readable input = new InputStreamReader(System.in);
--- test/TicTacToeControllerTest.java
+++ test/TicTacToeControllerTest.java
@@ -33,32 +33,411 @@ public class TicTacToeControllerTest {
   }
 
   @Test
-  public void testFoo() {
-    // Testing when something goes wrong with the Appendable
-    // Here we are passing it a mock of an Appendable that always fails
+  public void testRowOutOfBound() {
     TicTacToe m = new TicTacToeModel();
-    StringReader input = new StringReader("0 0 1 0 0 1 1 1 0 2 1 2");
-    Appendable output = System.out;
+    StringReader input = new StringReader("1 1 4 1 q");
+    Appendable output = new StringBuffer();
     TicTacToeController c = new TicTacToeConsoleController(input, output);
     c.playGame(m);
-    assertTrue(m.isGameOver());
-    assertTrue(m.getWinner() == Player.X);
+    StringBuffer stringBuffer = new StringBuffer("   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "Enter a move for X:\n" 
+        + " X |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "Enter a move for O:\n" 
+        + "Not a valid move: 4, 1\n" 
+        + "Game quit! Ending game state:\n" 
+        + " X |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  ");
+    assertEquals(stringBuffer.toString(), output.toString());
+  }
+
+  @Test
+  public void testColOutOfBound() {
+    TicTacToe m = new TicTacToeModel();
+    StringReader input = new StringReader("1 1 1 4 q");
+    Appendable output = new StringBuffer();
+    TicTacToeController c = new TicTacToeConsoleController(input, output);
+    c.playGame(m);
+    StringBuffer stringBuffer = new StringBuffer("   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "Enter a move for X:\n" 
+        + " X |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "Enter a move for O:\n" 
+        + "Not a valid move: 1, 4\n" 
+        + "Game quit! Ending game state:\n" 
+        + " X |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  ");
+    assertEquals(stringBuffer.toString(), output.toString());
+  }
+
+  @Test
+  public void testInvalidRow() {
+    TicTacToe m = new TicTacToeModel();
+    StringReader input = new StringReader("1 1 aaa 1 q");
+    Appendable output = new StringBuffer();
+    TicTacToeController c = new TicTacToeConsoleController(input, output);
+    c.playGame(m);
+    StringBuffer stringBuffer = new StringBuffer( "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "Enter a move for X:\n" 
+        + " X |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "Enter a move for O:\n" 
+        + "Not a valid number: aaa\n" 
+        + "Game quit! Ending game state:\n" 
+        + " X |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "-----------\n" 
+        "   |   |  ");
+    assertEquals(stringBuffer.toString(), output.toString());
+  }
+
+  @Test
+  public void testInvalidCol() {
+    TicTacToe m = new TicTacToeModel();
+    StringReader input = new StringReader("1 1 1 aaa q");
+    Appendable output = new StringBuffer();
+    TicTacToeController c = new TicTacToeConsoleController(input, output);
+    c.playGame(m);
+    StringBuffer stringBuffer = new StringBuffer( "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "Enter a move for X:\n" 
+        + " X |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "Enter a move for O:\n" 
+        + "Not a valid number: aaa\n" 
+        + "Game quit! Ending game state:\n" 
+        + " X |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  ");
+    assertEquals(stringBuffer.toString(), output.toString());
+
+    m = new TicTacToeModel();
+    input = new StringReader("1 1 2 2 1 q");
+    output = new StringBuffer();
+    c = new TicTacToeConsoleController(input, output);
+    c.playGame(m);
+    stringBuffer = new StringBuffer( "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "Enter a move for X:\n" 
+        + " X |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "Enter a move for O:\n" 
+        + " X |   |  \n" 
+        + "-----------\n" 
+        + "   | O |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "Enter a move for X:\n" 
+        + "Game quit! Ending game state:\n" 
+        + " X |   |  \n" 
+        + "-----------\n" 
+        + "   | O |  \n" 
+        + "-----------\n" 
+        + "   |   |  ");
+    assertEquals(stringBuffer.toString(), output.toString());
+  }
+
+
+  @Test
+  public void testValidMove() {
+    TicTacToe m = new TicTacToeModel();
+    StringReader input = new StringReader("1 1 q");
+    Appendable output = new StringBuffer();
+    TicTacToeController c = new TicTacToeConsoleController(input, output);
+    c.playGame(m);
+    StringBuffer stringBuffer = new StringBuffer( "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "Enter a move for X:\n" 
+        + " X |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "Enter a move for O:\n" 
+        + "Game quit! Ending game state:\n" 
+        + " X |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  ");
+    assertEquals(stringBuffer.toString(), output.toString());
+  }
+
+  @Test
+  public void testOccupied() {
+    TicTacToe m = new TicTacToeModel();
+    StringReader input = new StringReader("1 1 1 1 q");
+    Appendable output = new StringBuffer();
+    TicTacToeController c = new TicTacToeConsoleController(input, output);
+    c.playGame(m);
+    StringBuffer stringBuffer = new StringBuffer( "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "Enter a move for X:\n" 
+        + " X |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "Enter a move for O:\n" 
+        + "Not a valid move: 1, 1\n" 
+        + "Game quit! Ending game state:\n" 
+        + " X |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  ");
+    assertEquals(stringBuffer.toString(), output.toString());
+  }
+
+  @Test
+  public void testRunAfterInvalid() {
+    TicTacToe m = new TicTacToeModel();
+    StringReader input = new StringReader("1 1 1 aa 2 q");
+    Appendable output = new StringBuffer();
+    TicTacToeController c = new TicTacToeConsoleController(input, output);
+    c.playGame(m);
+    StringBuffer stringBuffer = new StringBuffer( "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "Enter a move for X:\n" 
+        + " X |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "Enter a move for O:\n" 
+        + "Not a valid number: aa\n" 
+        + " X | O |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "Enter a move for X:\n" 
+        + "Game quit! Ending game state:\n" 
+        + " X | O |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  ");
+    assertEquals(stringBuffer.toString(), output.toString());
+  }
+
+  @Test
+  public void testXWin() {
+    TicTacToe m = new TicTacToeModel();
+    StringReader input = new StringReader("1 1 2 1 1 2 2 2 1 3 2 3");
+    Appendable output = new StringBuffer();
+    TicTacToeController c = new TicTacToeConsoleController(input, output);
+    c.playGame(m);
+    StringBuffer stringBuffer = new StringBuffer( "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "Enter a move for X:\n" 
+        + " X |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "Enter a move for O:\n" 
+        + " X |   |  \n" 
+        + "-----------\n" 
+        + " O |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "Enter a move for X:\n" 
+        + " X | X |  \n" 
+        + "-----------\n" 
+        + " O |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "Enter a move for O:\n" 
+        + " X | X |  \n" 
+        + "-----------\n" 
+        + " O | O |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "Enter a move for X:\n" 
+        + " X | X | X\n" 
+        + "-----------\n" 
+        + " O | O |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "Game is over! X wins.");
+    assertEquals(stringBuffer.toString(), output.toString());
+  }
+
+
+  @Test
+  public void testOWin() {
+    TicTacToe m = new TicTacToeModel();
+    StringReader input = new StringReader("1 1 2 1 1 2 2 2 3 3 2 3");
+    Appendable output = new StringBuffer();
+    TicTacToeController c = new TicTacToeConsoleController(input, output);
+    c.playGame(m);
+    StringBuffer stringBuffer = new StringBuffer( "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "Enter a move for X:\n" 
+        + " X |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "Enter a move for O:\n" 
+        + " X |   |  \n" 
+        + "-----------\n" 
+        + " O |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "Enter a move for X:\n" 
+        + " X | X |  \n" 
+        + "-----------\n" 
+        + " O |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "Enter a move for O:\n" 
+        + " X | X |  \n" 
+        + "-----------\n" 
+        + " O | O |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "Enter a move for X:\n" 
+        + " X | X |  \n" 
+        + "-----------\n" 
+        + " O | O |  \n" 
+        + "-----------\n" 
+        + "   |   | X\n" 
+        + "Enter a move for O:\n" 
+        + " X | X |  \n" 
+        + "-----------\n" 
+        + " O | O | O\n" 
+        + "-----------\n" 
+        + "   |   | X\n" 
+        + "Game is over! O wins.");
+    assertEquals(stringBuffer.toString(), output.toString());
+  }
+  
+  @Test
+  public void testTie() {
+    TicTacToe m = new TicTacToeModel();
+    StringReader input = new StringReader("1 1 2 1 1 2 2 2 3 3 1 3 2 3 3 2 3 1");
+    Appendable output = new StringBuffer();
+    TicTacToeController c = new TicTacToeConsoleController(input, output);
+    c.playGame(m);
+    StringBuffer stringBuffer = new StringBuffer("   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "Enter a move for X:\n" 
+        + " X |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "Enter a move for O:\n" 
+        + " X |   |  \n" 
+        + "-----------\n" 
+        + " O |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "Enter a move for X:\n" 
+        + " X | X |  \n" 
+        + "-----------\n" 
+        + " O |   |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "Enter a move for O:\n" 
+        + " X | X |  \n" 
+        + "-----------\n" 
+        + " O | O |  \n" 
+        + "-----------\n" 
+        + "   |   |  \n" 
+        + "Enter a move for X:\n" 
+        + " X | X |  \n" 
+        + "-----------\n" 
+        + " O | O |  \n" 
+        + "-----------\n" 
+        + "   |   | X\n" 
+        + "Enter a move for O:\n" 
+        + " X | X | O\n" 
+        + "-----------\n" 
+        + " O | O |  \n" 
+        + "-----------\n" 
+        + "   |   | X\n" 
+        + "Enter a move for X:\n" 
+        + " X | X | O\n" 
+        + "-----------\n" 
+        + " O | O | X\n" 
+        + "-----------\n" 
+        + "   |   | X\n" 
+        + "Enter a move for O:\n" 
+        + " X | X | O\n" 
+        + "-----------\n" 
+        + " O | O | X\n" 
+        + "-----------\n" 
+        + "   | O | X\n" 
+        + "Enter a move for X:\n" 
+        + " X | X | O\n" 
+        + "-----------\n" 
+        + " O | O | X\n" 
+        + "-----------\n" 
+        + " X | O | X\n" 
+        + "Game is over! Tie game.");
+    assertEquals(stringBuffer.toString(), output.toString());
   }
 }
-
-
-// 2 2
-// 2 2 2
-// 1 hh
-// hh 1
-// hh hh
-// hhhh
-// ' '
-// '
-// 1
-//  1
-// 1    1
-// 1 .. 1
-// 1 q
-// q 1
-// q
